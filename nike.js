9863c408fa70664778a92b56050738875c4d8fc5
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-12-20 15:17:58 -0800

    test to see if it opens a page

--- nike.js
+++ nike.js
@@ -1,2 +1,20 @@
 const puppeteer = require("puppeteer");
-require("dotenv")
+require("dotenv").config();
+(async () => {
+
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://www.nike.com/ca/')
+
+  await page.waitForNetworkIdle();
+
+
+  const clickHandler = async (path) => {
+    const target = await page.$x(path);
+    await page.waitForNetworkIdle();
+    await target[0].click();
+    await page.waitForNetworkIdle();
+  };
+
+
+})();
