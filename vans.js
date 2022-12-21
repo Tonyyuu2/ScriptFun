976a4b7ab16011e9930726d7a10d73f23aa6b778
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-12-21 15:15:55 -0800

    changed from nike to vans

--- vans.js
+++ vans.js
@@ -1,13 +1,11 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 (async () => {
-
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.nike.com/ca/')
+  await page.goto("https://www.vans.ca/en-ca", { timeout: 0 });
 
-  await page.waitForNetworkIdle();
-
+  page.setDefaultNavigationTimeout(0);
 
   const clickHandler = async (path) => {
     const target = await page.$x(path);
@@ -17,4 +15,6 @@ require("dotenv").config();
   };
 
 
+
+  await page.waitForNetworkIdle();
 })();
